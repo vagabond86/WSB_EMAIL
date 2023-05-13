3b3bde9b906f59618e5193730b75c5a7ecfc4522
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-05-13 08:34:30 +0200

    - kontroler zwracający index - index / widok formularza do wysyłki maila

---
+++ src/main/java/wsb/email/wsb_email/MailController.java
@@ -0,0 +1,15 @@
+package wsb.email.wsb_email;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mails")
+public class MailController {
+
+    @GetMapping
+    String index(){
+        return "index";
+    }
+}
