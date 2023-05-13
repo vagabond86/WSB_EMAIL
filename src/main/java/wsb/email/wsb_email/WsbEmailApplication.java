031ffabad426f199653834838a6325c852c6f23b
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-05-13 08:17:02 +0200

    init

---
+++ src/main/java/wsb/email/wsb_email/WsbEmailApplication.java
@@ -0,0 +1,13 @@
+package wsb.email.wsb_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/email/wsb_email/WsbEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.email.wsb_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
