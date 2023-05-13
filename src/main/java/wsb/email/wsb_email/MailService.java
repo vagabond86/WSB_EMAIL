70e331903296290334f2bd87b496db23a4c4d05e
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-05-13 09:09:44 +0200

    - send service

--- src/main/java/wsb/email/wsb_email/MailService.java
+++ src/main/java/wsb/email/wsb_email/MailService.java
@@ -1,11 +1,33 @@
 package wsb.email.wsb_email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MailService {
 
-    void sendMail(Mail mail){
+    final private JavaMailSender javaMailSender;
+
+    void sendMail(Mail mail) {
+
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setTo(mail.getRecipient());
+            mimeMessageHelper.setSubject(mail.getSubject());
+            mimeMessageHelper.setText(mail.getContent());
+
+            javaMailSender.send(mimeMessage);
+
+        } catch (MessagingException e) {
+            System.out.println("Nie mogę wysłać maila, spróbuj później");
+        }
+
 
     }
 }
